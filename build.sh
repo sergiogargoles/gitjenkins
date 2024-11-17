a4e0de5d194e4b6c890e80634b0ab4ebddb008de
sergiogargoles <145029587+sergiogargoles@users.noreply.github.com>
2024-11-17 17:38:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 mkdir salida
 echo "Carpeta 'Salida' creada"
 
@@ -10,5 +12,15 @@ echo "Creando la página pagina2.html en salida..."
 echo "Creando la página pagina3.html en salida..."
 ./pagina3.sh > ./salida/pagina3.html
 echo "Añadiendo el archivo de estilos..."
-./w3.css.sh > ./salida/w3.css
+if [ -f ./salida/"w3.css"]; then
+    echo "Archivo CSS ya descargado."
+    else
+    curl - o "w3.css" "https://www.w3schools.com/w3css/4/w3.css"
+        if [$? -eq 0]; then 
+            echo "El archivo CSS se ha descargado correctamente"
+            mv "w3.css" ./salida
+            else
+            echo "Error en la descarga"
+        fi
+fi
 echo "Creado sin problemas"
