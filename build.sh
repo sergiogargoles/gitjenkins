21d747d8a11cd967962596e48fc8ec1a81473cfb
sergiogargoles <145029587+sergiogargoles@users.noreply.github.com>
2024-11-17 20:05:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
 mkdir salida
-echo "Carpeta 'Salida' creada"
 
-echo "Creando la página inicio.html en salida..."
+
+
 ./inicio.sh > ./salida/index.html
-echo "Creando la página pagina1.html en salida..."
+
 ./pagina1.sh > ./salida/pagina1.html
-echo "Creando la página pagina2.html en salida..."
+
 ./pagina2.sh > ./salida/pagina2.html
-echo "Creando la página pagina3.html en salida..."
+
 ./pagina3.sh > ./salida/pagina3.html
-echo "Añadiendo el archivo de estilos..."
+
 if [ -f ./salida/"w3.css"]; then
     echo "Archivo CSS ya descargado."
     else
